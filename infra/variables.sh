be8b66faa336f3cd2d5b794577235305d3b68c4d
Yuan LIN <ylin@dicksondata.fr>
2026-03-26 22:36:47 +0100

    update

--- infra/variables.sh
+++ infra/variables.sh
@@ -16,25 +16,25 @@ RESOURCE_GROUP="RG-Student-04"
 APP_SERVICE_LOCATION="westeurope"
 
 # App Service Plan
-APP_SERVICE_PLAN="HelloWorld-AppServicePlan"
+APP_SERVICE_PLAN="HelloWorld-Yuan-AppServicePlan"
 SKU="F1"
 
 # App Service
-APP_SERVICE_NAME="HelloWorld-AppService"
+APP_SERVICE_NAME="HelloWorld-Yuan-AppService"
 RUNTIME="DOTNETCORE:10.0"
 # Activate application logging and set log level (optional)
 APP_LOG_ENABLED="true"
 APP_LOG_LEVEL="information"  # verbose, information, warning, error
 
 # SQL Server
-SQL_SERVER_NAME="HelloWorld-SqlServer"  # Must be globally unique
+SQL_SERVER_NAME="HelloWorld-Yuan-SqlServer"  # Must be globally unique
 SQL_LOCATION="francecentral"
 SQL_ADMIN_USER="dbserveradmin"
 SQL_ADMIN_PASSWORD=""  # Set via environment variable or pass as argument
 SQL_ENTRA_ADMIN_EMAIL="yuan.lin@believeit.fr"  # Set to the Entra user/group email to be SQL admin
 
 # SQL Database
-SQL_DATABASE_NAME="HelloWorldDb"
+SQL_DATABASE_NAME="HelloWorld-Yuan-Db"
 SQL_SKU="Basic"
 SQL_USE_ELASTIC_POOL="false"
 SQL_ELASTIC_POOL_NAME=""
