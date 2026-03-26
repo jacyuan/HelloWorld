760dfc5e46b232a11fe72c2c7f06d505d308661b
Yuan LIN <ylin@dicksondata.fr>
2026-03-26 16:37:03 +0100

    models init

---
+++ HelloWorld/Models/Log.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld.Models;
+
+public partial class Log
+{
+    public int Id { get; set; }
+
+    public DateTime LogDate { get; set; }
+
+    public string Message { get; set; } = null!;
+}
